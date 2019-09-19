e50e9d0086bbec67dca501d284467cfc3bf7a00d
Mohan Liu <mohan@u.northwestern.edu>
2019-09-18 21:27:27 -0500

    New function; fix bugs

--- qmpy_rester/rester.py
+++ qmpy_rester/rester.py
@@ -84,6 +84,31 @@ class QMPYRester(object):
 
         return self._make_requests('/oqmdapi/formationenergy?%s'%_url)
 
+    def get_oqmd_phase_space(self, space, verbose=True, **kwargs):
+        """
+        Input:
+            space: str
+                e.g. 'Al-O', 'Pd-Se-Te-Pb'
+            verbose: boolean
+            **kwargs: dict
+        Output:
+            dict
+        """
+        _url = 'composition={}'.format(space)
+        _url += '&fields=name,delta_e,stability'
+        _url += '&limit=200'
+        output_data = self._make_requests('/oqmdapi/formationenergy?%s'%_url)
+
+        while output_data['links']['next']:
+            new_data = self._make_requests(output_data['links']['next'].replace(self.preamble, ''))
+            output_data['data'].extend(new_data['data'])
+            output_data['links']['next'] = new_data['links']['next']
+            output_data['meta']['data_returned'] += new_data['meta']['data_returned']
+
+        output_data['meta']['more_data_available'] = False
+
+        return output_data
+
     def get_oqmd_phase_by_id(self, fe_id, fields=None):
         if fields:
             if '!' in fields:
@@ -162,121 +187,3 @@ class QMPYRester(object):
                 return self._make_requests('/optimade/structures/%d?fields=%s'%(id, fields))
 
         return self._make_requests('/optimade/structures/%d'%id)
-
-    def get_entries(self, verbose=True, all_data=False, **kwargs):
-        """
-        Input:
-            verbose: boolean
-            all_data: boolean  
-                    :: whether or not to output all data at one time
-            **kwargs: dict
-                :composition
-                :calculated
-                :icsd
-                :band_gap
-                :ntypes
-                :generic
-                :sort_by
-                :desc
-                :sort_offset
-                :limit
-                :offset
-        Output:
-            dict
-        """
-        url_args = []
-        kwargs_list = ['composition', 'calculated', 'icsd',
-                       'band_gap', 'ntypes', 'generic',
-                       'sort_by', 'desc', 'sort_offset',
-                       'limit', 'offset']
-
-        for k in kwargs_list:
-            if k in kwargs:
-                url_args.append('%s=%s' %(k, kwargs[k]))
-
-        if verbose:
-            print("Your Entry filters are:")
-            if url_args == []:
-                print("   No filters?")
-            else:
-                for arg in url_args:
-                    print("   ", arg)
-
-            ans = input('Proceed? [Y/n]:')
-
-            if ans not in ['Y', 'y', 'Yes', 'yes']:
-                return
-
-        _url = '&'.join(url_args)
-
-        if all_data == True:
-            output = self._make_requests('/entry?%s'%_url)
-            next_page = output['next']
-            while next_page:
-                tmp = self._make_requests(next_page.replace(self.preamble, ''))
-                output['results'].extend(tmp['results'])
-                next_page = tmp['next']
-            output['next'] = next_page
-            return output
-            
-        return self._make_requests('/oqmdapi/entry?%s'%_url)
-
-    def get_entry_by_id(self, entry_id):
-        return self._make_requests('/oqmdapi/entry/%d'%entry_id)
-
-    def get_calculations(self, verbose=True, all_data=False, **kwargs):
-        """
-        Input:
-            verbose: boolean
-            all_data: boolean  
-                    :: whether or not to output all data at one time
-            **kwargs: dict
-                :converged
-                :label
-                :band_gap
-                :sort_by
-                :desc
-                :sort_offset
-                :limit
-                :offset
-        Output:
-            dict
-        """
-        url_args = []
-        kwargs_list = ['converged', 'label', 'band_gap',
-                       'sort_by', 'desc', 'sort_offset',
-                       'limit', 'offset']
-
-        for k in kwargs_list:
-            if k in kwargs:
-                url_args.append('%s=%s' %(k, kwargs[k]))
-
-        if verbose:
-            print("Your Calculation filters are:")
-            if url_args == []:
-                print("   No filters?")
-            else:
-                for arg in url_args:
-                    print("   ", arg)
-
-            ans = input('Proceed? [Y/n]:')
-
-            if ans not in ['Y', 'y', 'Yes', 'yes']:
-                return
-
-        _url = '&'.join(url_args)
-
-        if all_data == True:
-            output = self._make_requests('/oqmdapi/calculation?%s'%_url)
-            next_page = output['next']
-            while next_page:
-                tmp = self._make_requests(next_page.replace(self.preamble, ''))
-                output['results'].extend(tmp['results'])
-                next_page = tmp['next']
-            output['next'] = next_page
-            return output
-
-        return self._make_requests('/oqmdapi/calculation?%s'%_url)
-
-    def get_calculation_by_id(self, calc_id):
-        return self._make_requests('/oqmdapi/calculation/%d'%calc_id)
